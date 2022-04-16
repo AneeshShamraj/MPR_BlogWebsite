950585fa7633d5b0bc2e83be685782f365cf6a85
Hritik Sharma <sharmahritik2002>
2022-04-17 00:32:42 +0530

    Fix CSS issues

--- react-frontend/src/Components/BlogDetail/index.js
+++ react-frontend/src/Components/BlogDetail/index.js
@@ -3,7 +3,6 @@ import { useParams } from "react-router";
 import "./styles.css";
 import axios from "axios";
 
-
 const BlogDetail = () => {
   const { id } = useParams();
   const [blogTitle, setBlogTitle] = useState("");
@@ -15,40 +14,32 @@ const BlogDetail = () => {
   //       setBlog(json);
   //     });
   // }, []);
-  useEffect(()=>{
-    axios.get("http://localhost:5000/home/getblog/"+id)
-  .then(function (response) {
-    console.log(response.data);
-    setBlogTitle(response.data.title);
-    setBlogContent(response.data.content);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  },[]);
-  if(blogContent!=="" && blogTitle!==""){
-  return (
-    <div className="BlogStyle">
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/home/getblog/" + id)
+      .then(function (response) {
+        console.log(response.data);
+        setBlogTitle(response.data.title);
+        setBlogContent(response.data.content);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  if (blogContent !== "" && blogTitle !== "") {
+    return (
+      <div className="BlogStyle">
+        <div className="BlogDetail_wrapper">
+          <h1>{blogTitle}</h1>
+        </div>
 
-      <div className="BlogDetail_wrapper">
-        <h1>{blogTitle}</h1>
+        <div className="blog_body-content">
+          <p className="lead">{blogContent}</p>
+        </div>
       </div>
-
-
-      
-            <div className="col-lg-9 col-md-9 col-sm-12">
-              <p className="lead">
-                {blogContent}
-              </p>
-              
-            </div>
-          </div>
-        
-  );
-}
-else{
-  return(<div>
-  </div>);
-}
+    );
+  } else {
+    return <div>No Blog</div>;
+  }
 };
 export default BlogDetail;
--- react-frontend/src/Components/UserDashboard/UserDashboard.js
+++ react-frontend/src/Components/UserDashboard/UserDashboard.js
@@ -10,8 +10,8 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export const UserDashboard = () => {
-  if(!localStorage.token){
-    window.location="/";
+  if (!localStorage.token) {
+    window.location = "/";
     alert("User must be signed in to access this");
   }
   const customStyles = {
@@ -30,7 +30,6 @@ export const UserDashboard = () => {
   const [blogs, setBlogs] = useState([]);
   const [deletedId, setDeleteId] = useState("");
 
-
   // sample home page data
   useEffect(() => {
     // fetch("http:localhost:5000/home/userposts")
@@ -39,33 +38,37 @@ export const UserDashboard = () => {
     //     setBlogs(json);
     //     console.log(blogs);
     //   });
-    axios.get("http://localhost:5000/home/userposts", {headers: {
-      token: localStorage.token
-    }})
-    .then((res)=>{
-      console.log(res);
-      setBlogs(res.data);
-    })
-    .catch(err =>{
-      if(err.response.status===400){
-        alert(err.response.data);
-      }
-  });
-}, []);
+    axios
+      .get("http://localhost:5000/home/userposts", {
+        headers: {
+          token: localStorage.token,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setBlogs(res.data);
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          alert(err.response.data);
+        }
+      });
+  }, []);
   console.log("blogs:", blogs);
-
-
 
   useEffect(() => {
     setBlogs(blogs.filter((blog) => blog.id !== deletedId));
-    const body={blogID:deletedId}
-    axios.post("http://localhost:5000/users/deleteblog",body, {headers: {token: localStorage.token}})
-      .then(res =>{
-        console.log(res);
-        window.location='/user-dashboard';
+    const body = { blogID: deletedId };
+    axios
+      .post("http://localhost:5000/users/deleteblog", body, {
+        headers: { token: localStorage.token },
       })
-      .catch(err =>{
-        if(err.response.status===400){
+      .then((res) => {
+        console.log(res);
+        window.location = "/user-dashboard";
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
           // handle kr le bsdk
           // alert(err.response.data);
         }
@@ -81,54 +84,29 @@ export const UserDashboard = () => {
               className="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box"
               id="navigation"
             >
-              <div className="navi">
-                <ul>
-                  {/* <li className="active">
-                    <a href="#">
-                      <i className="fa fa-bar-chart" aria-hidden="true"></i>
-                      <span className="hidden-xs hidden-sm">User Dashboard</span>
-                    </a>
-                  </li> */}
-                  {/* <li> */}
-                  <a href="create">
-                    {/* <i className="fa fa-tasks" aria-hidden="true"></i>
-                      <span className="hidden-xs hidden-sm">Create Blog</span> */}
-
-                    {/* </li> */}
-                    <button
-                      type="button"
-                      className="btn btn-dark btn-secondary btn-lg btn-block"
-                    >
-                      Create Blog
-                    </button>
-                  </a>
-                  {/* <li>
-                    <a href="/">
-                      <i className="fa fa-home" aria-hidden="true"></i>
-                      <span className="hidden-xs hidden-sm">Home</span>
-                    </a>
-                  </li> */}
-
-                  {/* <li> */}
-                  <a href="profile">
-                    {/* <i className="fa fa-user" aria-hidden="true"></i>
-                      <span className="hidden-xs hidden-sm">Profile</span> */}
-
-                    {/* </li> */}
-                    <button
-                      type="button"
-                      className="btn btn-dark btn btn-secondary btn-lg btn-block"
-                    >
-                      Profile
-                    </button>
-                  </a>
-                  {/* <li>
+              <div className="navi__userDashboard">
+                <a href="create">
+                  <button
+                    type="button"
+                    className="btn btn-dark btn-secondary btn-lg btn-block"
+                  >
+                    Create Blog
+                  </button>
+                </a>
+                <a href="profile">
+                  <button
+                    type="button"
+                    className="btn btn-dark btn btn-secondary btn-lg btn-block"
+                  >
+                    Profile
+                  </button>
+                </a>
+                {/* <li>
                     <a href="#">
                       <i className="fa fa-cog" aria-hidden="true"></i>
                       <span className="hidden-xs hidden-sm">Setting</span>
                     </a>
                   </li> */}
-                </ul>
               </div>
             </div>
             {/* <div className="col-md-10 col-sm-11 display-table-cell v-align">
@@ -160,15 +138,15 @@ export const UserDashboard = () => {
                 .map((val, index) => (
                   <>
                     <Blogpost id={val._id} key={val._id} blog={val} />
-                    {/* <div><i className="fa-solid fa-pen-to-square"></i></div> */}
-                    <Link to={{ pathname: `edit/${val._id}`, param1: blogs }}>
-                      <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0 edit-btn">
-                          EDIT
-                        </button>
-                      </div>
-                    </Link>
+
                     <div className="input-field">
+                      <Link to={{ pathname: `edit/${val._id}`, param1: blogs }}>
+                        <div className="input-field">
+                          <button className="btn pink lighten-1 z-depth-0 edit-btn">
+                            EDIT
+                          </button>
+                        </div>
+                      </Link>
                       <button
                         className="btn pink lighten-1 z-depth-0 edit-btn"
                         onClick={() => {
