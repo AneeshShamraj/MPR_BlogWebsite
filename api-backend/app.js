6dbc575a1a26654baee752323739edf98951d69e
Aneesh Shamraj <aneesh.shamraj@gmail.com>
2022-04-30 10:26:10 +0530

    Add procfile

--- api-backend/app.js
+++ api-backend/app.js
@@ -15,7 +15,11 @@ mongoose.connect("mongodb+srv://admin:admin@cluster0.lb5fb.mongodb.net/projectDb
 app.use("/auth", require("./routes/auth"));
 app.use("/users",require("./routes/users"));
 app.use("/home",require("./routes/home"));
+let port=process.env.PORT;
 
-app.listen(5000,()=>{
-    console.log("Server started on port 5000");
+if(port==null || port==""){
+    port=5000;
+}
+app.listen(port,()=>{
+    console.log("Server started");
 });
